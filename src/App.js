c4d40a5735c399b1dea733f8e4eabae790db2a5f
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-01-04 16:21:38 -0500

    routed navbar links

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import CreateProfile from './components/CreateProfile/CreateProfile';
+import DoctorList from './components/DoctorList/DoctorList';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
+        <Route path="/CreateProfile" exact component ={CreateProfile}/>
+        <Route path="/DoctorList" exact component ={DoctorList}/>
       </Router>
     </div>
   );
---
+++ src/components/DoctorList/DoctorList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./DoctorList.css";
+
+class DoctorList extends Component{
+    render(){
+        return(
+            <h1>DoctorList</h1>
+        )
+    }
+}
+
+export default DoctorList;
