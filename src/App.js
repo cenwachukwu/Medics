40bb3ffa231120c7c58d7b2f95e3b34aae226aff
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-01-04 16:24:33 -0500

    initialized the constructor

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import CreateProfile from './components/CreateProfile/CreateProfile';
 import DoctorList from './components/DoctorList/DoctorList';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar/>
-        <Route path="/CreateProfile" exact component ={CreateProfile}/>
-        <Route path="/DoctorList" exact component ={DoctorList}/>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Doctors: []
+    }  
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Router>
+          <Navbar/>
+          <Route path="/CreateProfile" exact component ={CreateProfile}/>
+          <Route path="/DoctorList" exact component ={DoctorList}/>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
