a5c3113713d490402143fee942c965fc7e6de1d3
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-01-04 23:19:03 -0500

    added some styling to the doctor page

--- src/components/Doctor/Doctor.js
+++ src/components/Doctor/Doctor.js
@@ -4,50 +4,85 @@ import './Doctor.css';
 function Doctor(props) {
     console.log(props)
     return (
-        <div>
-            {props.Doctors.map((doctor, index) => {
-                if (props.doctorid === doctor._id) {
-                    return (
-                        <div key={index + 0} >
-                            <h4>{doctor.profile[0].first_name} {doctor.profile[0].last_name}, {doctor.profile[0].title}</h4>
-                            <p>{doctor.profile[0].gender}</p>
-                            <p>{doctor.profile[0].bio}</p>
-                            {doctor.educations ? <div>{doctor.educations.map((education, index) => {
-                                return (
-                                    <div key={index + 0}>
-                                        <div>
-                                            <h2>{education.school}</h2>
-                                            <h2>{education.degree}</h2>
-                                        </div>
+        <div className="Doctor__container">
+            <div >
+                {props.Doctors.map((doctor, index) => {
+                    if (props.doctorid === doctor._id) {
+                        return (
+                            <div key={index + 0} className="Doctor__boxes">
+                                <div className="Doctor__boxes--profile">
+                                    <div className="Doctor__boxes--name">
+                                        <h4>{doctor.profile[0].first_name} {doctor.profile[0].last_name}, {doctor.profile[0].title} <span className="Doctor__boxes--nameSpan">({doctor.profile[0].gender})</span></h4>
                                     </div>
-                                )
-                            })}</div> : null}
-                            {doctor.licenses ? <div>{doctor.licenses.map((license, index) => {
-                                return (
-                                    <div key={index + 0}>
-                                        <div>
-                                            <h2>{license.state}</h2>
-                                            <h2>{license.number}</h2>
-                                        </div>
+                                    <div>
+                                        <p className="Doctor__boxes--description"> {doctor.profile[0].bio}</p>
                                     </div>
-                                )
-                            })}</div> : null}
-                            {doctor.specialties ? <div>{doctor.specialties.map((speciality, index) => {
-                                return (
-                                    <div key={index + 0}>
-                                        <div>
-                                            <h2>{speciality.name}</h2>
-                                            <h2>{speciality.description}</h2>
-                                            <h2>{speciality.actor}</h2>
+                                </div>
+                                {doctor.educations ? <div>{doctor.educations.map((education, index) => {
+                                    return (
+                                        <div key={index + 0}>
+                                            <div>
+                                                <h2>{education.school}</h2>
+                                                <h2>{education.degree}</h2>
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                            })}</div> : null}
-                        </div>
-                    )
-                }
-            })}
-        </div> 
+                                    )
+                                })}</div> : null}
+                                {doctor.licenses ? <div>{doctor.licenses.map((license, index) => {
+                                    return (
+                                        <div key={index + 0}>
+                                            <div>
+                                                <h2>{license.state}</h2>
+                                                <h2>{license.number}</h2>
+                                            </div>
+                                        </div>
+                                    )
+                                })}</div> : null}
+                                {doctor.specialties ? <div>{doctor.specialties.map((speciality, index) => {
+                                    return (
+                                        <div key={index + 0}>
+                                            <div>
+                                                <h2>{speciality.name}</h2>
+                                                <h2>{speciality.description}</h2>
+                                                <h2>{speciality.actor}</h2>
+                                            </div>
+                                        </div>
+                                    )
+                                })}</div> : null}
+                                {doctor.practices ? <div>{doctor.practices.map((practice, index) => {
+                                    return (
+                                        <div key={index + 0}>
+                                            <div>
+                                                <h2>{practice.name}</h2>
+                                                {practice.website ? <p>{practice.website}</p> : null}
+                                                <h2>{practice.visit_address[0].street}, {practice.visit_address[0].city}, {practice.visit_address[0].state} {practice.visit_address[0].zip}</h2>
+                                                <h2>{practice.phones[0].type}: {practice.phones[0].number}</h2>
+                                                <h2>language spoken: {practice.languages[0].name}</h2>
+                                            </div>
+                                        </div>
+                                    )
+                                })}</div> : null}
+
+                                <div className="Doctor__insurance--container">
+                                    {doctor.insurances ? <div className="Doctor__insurance">{doctor.insurances.map((insurance, index) => {
+                                        return (
+                                            <div key={index + 0} className="DoctorList__box">
+                                                <p className="Doctor__insurance--ptag">Category: {insurance.insurance_plan[0].category}</p>
+                                                <p className="Doctor__insurance--ptag">Insurance Company: {insurance.insurance_plan[0].name}</p>
+                                                <p className="Doctor__insurance--ptag">UID: {insurance.insurance_plan[0].uid}</p>
+                                            </div>
+                                        )
+                                    })}</div> : null}
+                                </div>
+
+
+
+                            </div>
+                        )
+                    }
+                })}
+            </div>
+        </div>
 
 
     )
