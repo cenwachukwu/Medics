447bdb6dd7f29bdddad6c803d7a14dfa9535eb83
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-01-04 22:02:07 -0500

    rendering the data from the api

--- src/components/Doctor/Doctor.js
+++ src/components/Doctor/Doctor.js
@@ -1,9 +1,55 @@
 import React from 'react';
 import './Doctor.css';
 
-function Doctor (props) {
-    return(
-        <h1>Doctor</h1>
+function Doctor(props) {
+    console.log(props)
+    return (
+        <div>
+            {props.Doctors.map((doctor, index) => {
+                if (props.doctorid === doctor._id) {
+                    return (
+                        <div key={index + 0} >
+                            <h4>{doctor.profile[0].first_name} {doctor.profile[0].last_name}, {doctor.profile[0].title}</h4>
+                            <p>{doctor.profile[0].gender}</p>
+                            <p>{doctor.profile[0].bio}</p>
+                            {doctor.educations ? <div>{doctor.educations.map((education, index) => {
+                                return (
+                                    <div key={index + 0}>
+                                        <div>
+                                            <h2>{education.school}</h2>
+                                            <h2>{education.degree}</h2>
+                                        </div>
+                                    </div>
+                                )
+                            })}</div> : null}
+                            {doctor.licenses ? <div>{doctor.licenses.map((license, index) => {
+                                return (
+                                    <div key={index + 0}>
+                                        <div>
+                                            <h2>{license.state}</h2>
+                                            <h2>{license.number}</h2>
+                                        </div>
+                                    </div>
+                                )
+                            })}</div> : null}
+                            {doctor.specialties ? <div>{doctor.specialties.map((speciality, index) => {
+                                return (
+                                    <div key={index + 0}>
+                                        <div>
+                                            <h2>{speciality.name}</h2>
+                                            <h2>{speciality.description}</h2>
+                                            <h2>{speciality.actor}</h2>
+                                        </div>
+                                    </div>
+                                )
+                            })}</div> : null}
+                        </div>
+                    )
+                }
+            })}
+        </div> 
+
+
     )
 }
 
--- src/components/DoctorList/DoctorList.js
+++ src/components/DoctorList/DoctorList.js
@@ -3,20 +3,23 @@ import { Link } from '@reach/router';
 import "./DoctorList.css";
 
 function DoctorList(props) {
-    console.log(props.Doctors)
+    // console.log(props.Doctors)
     return (
-        <div className="DoctorList__list">
-            {props.Doctors.map((doctor, index) => {
-                console.log(doctor)
-                return (
-                    <div key={index + 0} className="DoctorList__box">
-                        <h4>{doctor.profile[0].first_name} {doctor.profile[0].last_name}</h4>
-                        {doctor.specialties[0] ? <p className="DoctorList__box--specialties">{doctor.specialties[0].actor}</p> : null}
-                        <p><Link to={`/Doctor/${doctor._id}`}>View More</Link></p>
-                    </div>
-                )
-            })}
+        <div className="DoctorList__container">
+            <div className="DoctorList__list">
+                {props.Doctors.map((doctor, index) => {
+                    console.log(doctor)
+                    return (
+                        <div key={index + 0} className="DoctorList__box">
+                            <h4>{doctor.profile[0].first_name} {doctor.profile[0].last_name}, {doctor.profile[0].title}</h4>
+                            {doctor.specialties[0] ? <p className="DoctorList__box--specialties">{doctor.specialties[0].actor}</p> : null}
+                            <p><Link to={`/Doctor/${doctor._id}`}>View More</Link></p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
+
     )
 
 }
