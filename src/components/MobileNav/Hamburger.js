49818522ccfac6e01bd7e2c7286b3b2e14758ee2
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-01-06 15:22:04 -0500

    completed the navbar for now

--- src/components/MobileNav/Hamburger.js
+++ src/components/MobileNav/Hamburger.js
@@ -12,6 +12,23 @@ class Hamburger extends Component {
                     <div class="hamburger">
                         <div class="hamburger-lines"></div>
                     </div>
+                    <div class="fixednavbar">
+                        <div>
+                            <div>
+                                <ul>
+                                    <li>
+                                        <Link to="/" className="atag">Home</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/DoctorList" className="atag">Find your doctor</Link>
+                                    </li>
+                                    <li>
+                                    <Link to="/CreateProfile" className="atag">Doctors click here</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
