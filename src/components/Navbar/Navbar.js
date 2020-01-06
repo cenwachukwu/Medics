33fe90885d3c07b1ded7c664e90138bab851dce2
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-01-05 21:06:08 -0500

    added some styling to the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from '@reach/router';
+import '../../App.css';
 import "./Navbar.css";
 
 class Navbar extends Component {
     render() {
         return (
-            <div className="Navbar">
-               <div>
-                <Link to="/">
-                    <h4>Medicx</h4>
-                </Link>
-                </div>
-                <div className="NavLinks">
-                    <ul className="NavBar">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/DoctorList">Find your doctor</Link>
-                        </li>
-                        <li>
-                            <Link to="/CreateProfile">Doctors click here</Link>
-                        </li>
-                    </ul>
+            <div className="navContainer">
+                <div>
+                    {window.innerWidth < 769 ?
+                        <div></div> :
+                        <div className="navBarContainer">
+                            <div>
+                                <Link to="/" className="atag">
+                                    <h4 className="homeLogo">Medicx</h4>
+                                </Link>
+                            </div>
+                            <div className="NavLinks">
+                                <ul className="NavBarLinks">
+                                    <li >
+                                        <Link to="/" className="atag">Home</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/DoctorList" className="atag">Find your doctor</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/CreateProfile" className="atag">Doctors click here</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    }
                 </div>
             </div>
         )
