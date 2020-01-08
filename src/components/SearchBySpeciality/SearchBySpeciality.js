6d0dbc99fa85aaefca6e906908cfa14a089b95e6
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-01-08 15:43:26 -0500

    added search by speciality

--- src/components/SearchBySpeciality/SearchBySpeciality.js
+++ src/components/SearchBySpeciality/SearchBySpeciality.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
 import { Link } from '@reach/router';
+import Axios from 'axios';
 import '../../App.css';
-// import "./SearchDoctors.css";
+import '../Doctor/Doctor.css'
 
 class SearchBySpeciality extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: "", //to show the users query
+            result: [], //to show the results
+        };
+    }
+    fetchResultsFromApi = (query) => {
+        const Url = `https://med-directory-cen.herokuapp.com/directory/specialties/${query}`;
+        Axios.get(Url)
+            .then(res => {
+                console.log(res.data[0])
+                this.setState({
+                    result: res.data //now we set state of results to res.data[0]
+                })
+                console.log(this.state.result)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+    onProfileNameChange = (evt) => {
+        const query = evt.target.value;
+        console.log(query)
+        this.setState({ query: query }, () => {
+            this.fetchResultsFromApi(query);
+        });
+    }
+
     render() {
+        const { query, result } = this.state;
+        console.log(this.state)
         return (
-            <div className="SearchBySpeciality">
-                <div className="SearchBySpeciality__Container">
-                    <h2>SearchBySpeciality</h2>
+            <div className="SearchByLastName">
+                <div className="SSearchByLastName__Container">
+                    <input type="text" value={query} placeholder="Enter speciality eg. Surgery" onChange={this.onProfileNameChange} />
+                </div>
+                <div className="DoctorList__list">
+                    {this.state.result.map((doctor, index) => {
+                        console.log(doctor)
+                        return (
+                            <div key={index + 0} className="DoctorList__box">
+                                <h4>{doctor.profile[0].first_name} {doctor.profile[0].last_name}, {doctor.profile[0].title}</h4>
+                                {doctor.specialties[0] ? <p className="DoctorList__box--specialties">{doctor.specialties[0].actor}</p> : null}
+                                <p><Link to={`/Doctor/${doctor._id}`}>View More</Link></p>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         )
